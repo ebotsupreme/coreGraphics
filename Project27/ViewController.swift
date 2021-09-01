318df5de19f96236efdbd6e0186d8e7cf9016744
Eddie Jung <ejung716@gmail.com>
2021-09-01 15:24:25 -0700

    initial setup. Create iamgeView and button. Add redrawTapped func.

---
+++ Project27/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by Eddie Jung on 9/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var currentDrawType = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        drawRectangle()
+    }
+
+    @IBAction func redrawTapped(_ sender: UIButton) {
+        currentDrawType += 1
+        
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        default:
+            break
+        }
+    }
+    
+    func drawRectangle() {
+        
+    }
+}
+
